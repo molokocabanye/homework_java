791792e43fedee0c7acbb7d5578323ab0eafec64
victoria.n <125078247+molokocabanye@users.noreply.github.com>
2025-11-08 15:20:07 +0300

    Homework #3 add saving of park

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,5 +17,22 @@ public class Main {
         for (int i = 0; i < productsArray.length; i++) {
             System.out.println(productsArray[i].getInfo());
         }
+
+        Park.Attraction[] attractionsArray = new Park.Attraction[3];
+
+        attractionsArray[0] = new Park.Attraction("Колесо обозрения",
+                "10:00-22:00", 350);
+        attractionsArray[1] = new Park.Attraction("Американские горки",
+                "09:00-20:00", 500);
+        attractionsArray[2] = new Park.Attraction("Карусель",
+                "10:00-18:00", 200);
+
+        Park centralPark = new Park("Центральный парк", attractionsArray);
+
+        System.out.println("\n--- Информация о парке ---");
+        System.out.println("Парк: " + centralPark.parkName);
+        for (int i = 0; i < centralPark.attractions.length; i++) {
+            System.out.println(centralPark.attractions[i].getAttractionInfo());
+        }
     }
 }
