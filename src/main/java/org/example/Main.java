36e1e7d6aeb730c664b9fdc7a83b0b4e7a896cde
victoria.n <125078247+molokocabanye@users.noreply.github.com>
2025-11-14 14:43:34 +0100

    Homework #5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,112 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+class MyArraySizeException extends Exception {
+    MyArraySizeException(String msg) {
+        super(msg);
+    }
+}
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+class MyArrayDataException extends Exception {
+    int row;
+    int col;
+    String value;
+
+    MyArrayDataException(int r, int c, String v) {
+        super("В ячейке [" + r + "][" + c + "] лежит не число: '" + v + "'");
+        row = r;
+        col = c;
+        value = v;
+    }
+}
+
+public class Main {
+
+    static int sumArray(String[][] arr) throws MyArraySizeException, MyArrayDataException {
+        if (arr.length != 4) {
+            throw new MyArraySizeException("Нужно 4 строки, а есть " + arr.length);
         }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraySizeException("В строке " + i + " нужно 4 столбца, а есть " + arr[i].length);
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    result += Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j, arr[i][j]);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    static void showArrayIndexError() {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        try {
+            System.out.println("Берем элемент с индексом 10...");
+            int x = numbers[10];
+            System.out.println("Значение: " + x);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Ошибка! Такого индекса нет: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Тест 1 - Хороший массив:");
+        String[][] goodArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            int sum = sumArray(goodArray);
+            System.out.println("Сумма: " + sum);
+        } catch (Exception e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        System.out.println("\nТест 2 - Массив не 4x4:");
+        String[][] smallArray = {
+                {"1", "2"},
+                {"3", "4"}
+        };
+
+        try {
+            int sum = sumArray(smallArray);
+            System.out.println("Сумма: " + sum);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных: " + e.getMessage());
+        }
+
+        System.out.println("\nТест 3 - В массиве есть текст:");
+        String[][] textArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "семь", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            int sum = sumArray(textArray);
+            System.out.println("Сумма: " + sum);
+        } catch (MyArraySizeException e) {
+            System.out.println("Ошибка размера: " + e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка данных: " + e.getMessage());
+        }
+
+        System.out.println("\nТест 4 - ArrayIndexOutOfBoundsException:");
+        showArrayIndexError();
     }
 }
